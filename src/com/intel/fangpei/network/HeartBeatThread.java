0151c94bc422e22716dad62f11f87a202d2027ea
wsdmakeup <wsd.makeup@gmail.com>
2014-04-13 13:26:43 +0800

    heartbeat xml

--- src/com/intel/fangpei/network/HeartBeatThread.java
+++ src/com/intel/fangpei/network/HeartBeatThread.java
@@ -4,6 +4,7 @@ package com.intel.fangpei.network;
 import com.intel.fangpei.BasicMessage.BasicMessage;
 import com.intel.fangpei.BasicMessage.HeartBeatMessage;
 import com.intel.fangpei.BasicMessage.packet;
+import com.intel.fangpei.util.ConfManager;
 
 public class HeartBeatThread implements Runnable{
 	
@@ -12,7 +13,9 @@ public class HeartBeatThread implements Runnable{
 	private NIOHandler connect;
 	private String hostname;
 	public HeartBeatThread(NIOHandler connect,String hostname){
-		
+		ConfManager.addResource(null);
+		HEART_BEAT_INTERVAL= ConfManager.getInt("heartbeat.interval", HEART_BEAT_INTERVAL);
+		HEART_BEAT_OUT_TIME= ConfManager.getInt("heartbeat.outtime", HEART_BEAT_OUT_TIME);
 		this.connect=connect;
 		this.hostname=hostname;
 	}
